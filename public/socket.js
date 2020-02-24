90ca4bb9d19b55acde8d832b4c97022cfae8d7a9
nilshah98 <neel12@somaiya.edu>
2020-02-24 18:31:45 +0530

    [BUG] Added support for abrupt disconnect and ask before disconnect

--- public/socket.js
+++ public/socket.js
@@ -13,17 +13,24 @@ var stat = []
 // Parameter for workers
 var workerParams = []
 
+window.addEventListener('beforeunload', function (e) {
+    // Cancel the event
+    e.preventDefault();
+    // Chrome requires returnValue to be set
+    e.returnValue = '';
+});
+
 // to terminate a web worker
 const terminate = (id) => {
-
+    
     // Terminating a specific worker by accessing by id
     team[id].terminate()
-
+    
     // Setting the status of worker to 0
     stat[id] = 0
-
+    
     console.log("stat - ", stat)
-
+    
     // checking status of each thread, if it is 0 for all, emit processingDone
     if(stat.reduce((total,cur) => total+cur) === 0){
         console.log(`All threads have been terminated`)
@@ -35,31 +42,31 @@ const terminate = (id) => {
 }
 
 socket.on('initialize', (url) => {
-
+    
     // Need to close earlier workers and clear the worker team
     team.forEach((worker) => worker.terminate());
-
+    
     // Reset the worker team and their status
     team = [];
     stat = [];
-
+    
     // Need to create array of workers, and define their behaviour
     for(let i=0; i<cores; i++){
         var myWorker = new Worker(url);
-
+        
         // Assigning data as per cores, as that is what is received
         outputData[i] = [];
-
+        
         // Each worker will be allocated a fixed array of tasks to perform
         // So each worker, spawns and executes exactly one time
-
+        
         // Can optimize here, by dynamically calling postMessage for each onnessage
         myWorker.onmessage = (res) => {
             // Output in any format as needed
-
+            
             // Can optimize further by only giving dataIndex
             // And generating data on master node
-
+            
             console.log(`calculation done from ${workerParams[i][0]} to ${workerParams[i][1]}`);
             
             // Since each worker works exactly once
@@ -67,43 +74,43 @@ socket.on('initialize', (url) => {
             outputData[i] = res.data;
             terminate(i);
         }
-
+        
         // Finally pusing worker to the team
         team.push(myWorker);
         // Setting the status of worker to 1, as it is in use.
         stat.push(1);
     }
-
+    
     console.log('Team of myWorkers created as ->', team);
     // Sending ready message to master node, to receive range next
     socket.emit('ready');
 })
 
 socket.on('range', (start, step, fileLoc) => {
-
+    
     // Since data is really queues on the web worker side
     // We can just send the parameters to construct the input data
     // This will save communication costs as well
-
+    
     // Alternatively we can find a complex solution
     // That reponds to every postMessage from workerScript
-
+    
     // Creating starting parameters and step for each array
     // Ex. start -> 1000 step -> 300 cores -> 8
     // Need to divide as -> [1000 - 1037],[1038 - 1075],[1076 - 1113],[1114 - 1151]
     // [1152 - 1189],[1190 - 1227],[1228 - 1265],[1266 - 1300]
-
+    
     // generating params for each worker thread here
     // inputData is generated on the thread itself to save communication costs
     var workerStep = Math.ceil(step/cores)
-
+    
     for(let i=0; i<(cores-1); i++){
         workerParams.push([start + (i*workerStep), workerStep, fileLoc])
     }
-
+    
     // Last core will get all the remaining ones
     workerParams.push([start + ((cores-1)*workerStep), step - ((cores-1)*workerStep) + 1, fileLoc])
-
+    
     for(let i=0; i<cores; i++){
         console.log(`Thread ${i} is alloted ${workerParams[i][0]} to ${workerParams[i][0] + workerParams[i][1]}`)
         team[i].postMessage(workerParams[i])
